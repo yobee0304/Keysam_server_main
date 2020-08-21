01fb5c403d1732650f33c9262e42a479af2d417d
안찬영 <an20171@naver.com>
2020-08-21 18:00:44 +0900

    fcm 추가

--- src/main/java/com/example/keysam/Controller/CrawlingController.java
+++ src/main/java/com/example/keysam/Controller/CrawlingController.java
@@ -1,6 +1,8 @@
 package com.example.keysam.Controller;
 
 import com.example.keysam.db_Connection;
+import com.example.keysam.fcm;
+import org.apache.ibatis.jdbc.SQL;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +40,7 @@ public class CrawlingController {
                 // 결과 데이터를 JSON으로 변환하여 JSONArray에 저장
                 while(rs.next()){
                     JSONObject jo = new JSONObject();
+                    jo.put("cid", rs.getInt("cid"));
                     jo.put("sid", rs.getInt("sid"));
                     jo.put("keyword", rs.getString("keyword"));
                     jo.put("webpage", rs.getString("webpage"));
@@ -64,12 +67,17 @@ public class CrawlingController {
     @PostMapping("/spInsertArticle")
     public String insertArticle(@RequestParam("csv_file") MultipartFile file){
 
+        fcm fcm = new fcm();
+
         // 비어있는 파일인 경우
         if (file.isEmpty()) { ;
             return "file is empty";
         }else{
 
             String query = "{call sp_insert_article(?, ?)}";
+            String query_token = "{call sp_get_device_token(?)}";
+
+            CallableStatement stmt;
 
             try{
                 byte[] bytes = file.getBytes();
@@ -79,15 +87,57 @@ public class CrawlingController {
                 // 열 단위로 분리
                 String[] rows = completeData.split("\n");
 
+                // 사용자 정보
+                String device_token = null;
+                int cid = -1;
+                // 해당 사용자에 insert되는 article 개수
+                int article_cnt = 0;
+
                 for(int i=1; i<rows.length;i++){
                     String[] column = rows[i].split(",");
-                    int sid = Integer.parseInt(column[0]);
-                    String url = column[1];
+
+                    // 새로운 유저에게 article을 insert하는 경우
+                    // cid 및 device token 변경
+                    if (cid != Integer.parseInt(column[0])){
+
+                        // 첫 사용자가 아닌 모든 경우
+                        // 푸시 메시지 전송(FCM)
+                        // TODO title, message 추후 개선 예정
+                        if (cid > 0){
+                            String title = "푸시 메세지";
+                            String message = "새로운 게시물이 총 " + article_cnt + "건 추가되었습니다.";
+
+                            fcm.setConfig(title, message, device_token);
+                            fcm.pushMessage();
+                        }
+
+                        cid = Integer.parseInt(column[0]);
+                        article_cnt = 0;
+
+                        try{
+                            stmt = con.prepareCall(query_token);
+                            stmt.setInt(1, cid);
+
+                            stmt.execute();
+                            ResultSet rs = stmt.getResultSet();
+                            rs.next();
+                            device_token = rs.getString("device_token");
+
+                            stmt.close();
+                        }catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    int sid = Integer.parseInt(column[1]);
+                    String url = column[2];
+
+                    article_cnt++;
 
                     try
                     {
                         // sid, url을 sp 파라미터에 넣고 실행
-                        CallableStatement stmt = con.prepareCall(query);
+                        stmt = con.prepareCall(query);
                         stmt.setInt(1,sid);
                         stmt.setString(2,url);
 
--- src/main/java/com/example/keysam/KeysamApplication.java
+++ src/main/java/com/example/keysam/KeysamApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
